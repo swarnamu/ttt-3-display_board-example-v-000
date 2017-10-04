c8b70eeafbc34b48d6fe1c02489dfa724290c115
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-04 20:05:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,6 @@ def display_board
   space = "   "
   pipe = "|"
   dash = "-----------"
-  board = #space#pipe#space#pipe#space"\n"#dash"\n"#space#pipe#space#pipe#space"\n"#dash"\n"#space#pipe#space#pipe#space
+  board = #{space}#{pipe}#{space}#{pipe}#{space}"\n"#{dash}"\n"#{space}#{pipe}#{space}#{pipe}#{space}"\n"#{dash}"\n"#{space}#{pipe}#{space}#{pipe}#{space}
   return board
 end
